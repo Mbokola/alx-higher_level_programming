0fbdda17bc6adcaf228f0af923f003ff38fd78ae
Mbokola <tonypeaks@gmail.com>
2023-05-09 20:01:57 +0000

    Task 8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
     for c in str:
-        if (int(c) - 32 >= 65):
+        c = int(c)
+        if (c - 32 >= 65):
             c -= 32
         print("{}".format(chr(c)), end="")
