c97683b82668a6b78b222650f0aa105afdd991ad
Mbokola <tonypeaks@gmail.com>
2023-05-09 21:00:26 +0000

    Task 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 for i in range(9):
-    print(i, end="")
     for a in range(i + 1, 10):
-        if (i != 8 and a != 9):
-            print("{}, ".format(i))
+        print("{}{}".format(i, a), end="")
+        if (i != 8 or a != 9):
+            print(", ", end="")
+        a += 1
+    i += 1
