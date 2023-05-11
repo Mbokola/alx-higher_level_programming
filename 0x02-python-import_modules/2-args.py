2b5353478ffdfa064e8de0662c0fb25adedf8ad4
Mbokola <tonypeaks@gmail.com>
2023-05-11 20:19:45 +0000

    Task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,6 +5,9 @@ if (__name__ == '__main__'):
     if (args == 1):
         print('0 arguments.')
     else:
-        print(f"{args - 1} arguments:")
+        if (args == 2):
+            print(f"{args - 1} argument:")
+        else:
+            print(f"{args - 1} arguments:")
         for i in range(1, args):
             print(f"{i}: {sys.argv[i]}")
