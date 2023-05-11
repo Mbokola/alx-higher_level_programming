6badb3b0b7544cffbb29ded114abcefd44b88a6f
Mbokola <tonypeaks@gmail.com>
2023-05-11 20:12:12 +0000

    Task 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import sys
+if (__name__ == '__main__'):
+    args = len(sys.argv)
+    if (args == 1):
+        print('0 arguments.')
+    else:
+        print(f"{args - 1} arguments:")
+        for i in range(1, args):
+            print(f"{i}: {sys.argv[i]}")
