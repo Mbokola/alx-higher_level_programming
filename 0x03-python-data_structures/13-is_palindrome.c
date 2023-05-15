c0606adf0066814e49a2349c85568103e0d2ef22
Mbokola <tonypeaks@gmail.com>
2023-05-15 11:15:32 +0000

    Task 13

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -15,6 +15,8 @@ int is_palindrome(listint_t **head)
 	ptr = *head;
 	for (i = 0; ptr->next; i++)
 		ptr = ptr->next;
+	if (ptr->n == (*head)->n)
+		return (1);
 	ptr = *head;
 	j = i;
 	arr = malloc(sizeof(int) * i);
