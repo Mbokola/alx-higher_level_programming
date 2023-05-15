a555fe3a2c813389efbdf13046547771bd121258
Mbokola <tonypeaks@gmail.com>
2023-05-15 13:42:48 +0000

    8-multiple_returns.py

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    length = len(sentence)
+    if length:
+        my_tuple = (length, sentence[0])
+        return my_tuple
+    my_tuple = (length, None)
