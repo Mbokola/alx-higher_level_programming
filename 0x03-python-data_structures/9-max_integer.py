1efbb3ae6e62037ad078434cc83aae9488fc30f1
Mbokola <tonypeaks@gmail.com>
2023-05-15 13:26:46 +0000

    Task 9

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if (not my_list):
+    if not my_list or not len(my_list):
         return None
     sort_list = sorted(my_list)
     return (sort_list[-1])
