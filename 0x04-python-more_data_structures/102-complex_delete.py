447a9de3c8aa12d5443a2c02857ea933493faa9e
Mbokola <tonypeaks@gmail.com>
2023-05-17 19:07:30 +0000

    Task 15

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
+    keys_to_del = []
     for k, v in a_dictionary.items():
         if value == v:
-            del a_dictionary[k]
+            keys_to_del.append(k)
+    for k in keys_to_del:
+        del a_dictionary[k]
