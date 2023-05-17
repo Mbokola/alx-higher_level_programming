540e9e32ea9251f56aeed0239a0c760a9fc6343a
Mbokola <tonypeaks@gmail.com>
2023-05-17 19:25:54 +0000

    Task 15

--- 0x04-python-more_data_structures/test.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-complex_delete = __import__('102-complex_delete').complex_delete
-
-my_dict = {'a': 1, 'b': 2, 'c': 3, 'd': 2}
-print("Original dictionary:", my_dict)
-
-value_to_delete = 2
-complex_delete(my_dict, value_to_delete)
-print("After deleting", value_to_delete, ":", my_dict)
