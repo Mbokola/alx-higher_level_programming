d03d10e25fc6445f6ce386aa80c5f534ae15791e
Mbokola <tonypeaks@gmail.com>
2023-05-17 20:20:43 +0000

    Task 13

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,9 @@
+def weight_average(my_list=[]):
+    total = 0
+    weight = 0
+    if not my_list:
+        return 0
+    for i in my_list:
+        total += i[0] * i[1]
+        weight += i[1]
+    return total/weight
