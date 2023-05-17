cdc77e3547ba9ab1f2881f7b9effd2a8205e9b31
Mbokola <tonypeaks@gmail.com>
2023-05-17 22:37:03 +0000

    Task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
+    if a_dictionary is None:
+        return None
     top_score = 0
-    for key, score in a_dictionary:
+    for key, score in a_dictionary.items():
         if score > top_score:
             top_score = score
             alias = key
