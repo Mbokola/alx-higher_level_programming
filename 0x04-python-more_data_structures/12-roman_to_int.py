f89da52e0c76e8b212d19cd4852067b9ac80dc8c
Mbokola <tonypeaks@gmail.com>
2023-05-17 22:16:12 +0000

    Task 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,17 +2,16 @@
 def roman_to_int(roman_string):
     if (not len(roman_string)):
         return 0
-    roman_string = roman_string.upper()
     ref = dict(I=1, V=5, X=10, L=50, C=100, D=500, M=1000)
     total = 0
     counter = 0
     for i in roman_string:
+        i = i.upper()
         if i in ref:
             total += ref[i]
             if ref[i] > counter:
                 total -= counter * 2
-            else:
-                counter = ref[i]
+            counter = ref[i]
         else:
             return 0
     return total
