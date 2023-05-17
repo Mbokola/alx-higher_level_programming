ed026f2cd7e6722e004e795b3ffccc808d696c81
Mbokola <tonypeaks@gmail.com>
2023-05-17 17:58:57 +0000

    Task 16

---
+++ 0x04-python-more_data_structures/103-python.c
@@ -0,0 +1,79 @@
+#include <Python.h>
+#include <bytesobject.h>
+#include <listobject.h>
+
+/**
+ *print_python_bytes - parses byteobjects
+ *@p: object to parse
+ *
+ *Return: Nothing
+ */
+void print_python_bytes(PyObject *p)
+{
+	Py_ssize_t size;
+	const char *str;
+	int i;
+
+	printf("[.] bytes object info\n");
+	if (PyBytes_Check(p))
+	{
+		size = PyBytes_GET_SIZE(p);
+		printf("  size: %zd\n", size);
+		str = PyBytes_AS_STRING(p);
+		printf("  trying string: %s\n", str);
+		if (size < 10)
+			printf("  first %lu bytes: ", size + 1);
+		else
+			printf("  first 10 bytes: ");
+		for  (i = 0; size; i++)
+		{
+			if (i > 9)
+				break;
+			printf("%02hhx ", str[i]);
+			size--;
+		}
+		if (i < 9)
+			printf("00");
+		printf("\n");
+	}
+	else
+		printf("  [ERROR] Invalid Bytes Object\n");
+}
+
+/**
+ *print_python_list - parses and prints list object
+ *@p: listobject
+ *
+ *Return: Nothing
+ */
+void print_python_list(PyObject *p)
+{
+	Py_ssize_t size, alloc, i;
+	const char *typename;
+	PyListObject *listObj = (PyListObject *)p;
+	PyObject *item, *type, *name;
+
+	if (PyList_Check(p))
+	{
+		printf("[*] Python list info\n");
+		size = PyList_GET_SIZE(p);
+		printf("[*] Size of the Python List = %zd\n", size);
+		alloc = listObj->allocated;
+		printf("[*] Allocated = %zd\n", alloc);
+		if (size)
+		{
+			for (i = 0; i < size; i++)
+			{
+				item = PyList_GET_ITEM(p, i);
+				type = PyObject_Type(item);
+				name = PyObject_GetAttrString(type, "__name__");
+				typename = PyUnicode_AsUTF8(name);
+				printf("Element %zd: %s\n",i, typename);
+				if (!strcmp(typename, "bytes"))
+					print_python_bytes(item);
+
+			}
+		}
+	}
+
+}
