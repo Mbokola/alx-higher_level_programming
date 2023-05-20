c7b7b853fbd4ed5b5938fd7c731f9913c942a6d9
Mbokola <tonypeaks@gmail.com>
2023-05-20 17:39:19 +0000

    Task 6

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -10,5 +10,5 @@ Write a function that prints a dictionary by ordered keys.
 
 
 def print_sorted_dictionary(a_dictionary):
-    sorted_dict = {k: a_dictionary[k] for k in sorted(a_dictionary)}
-    print(sorted_dict, end='\n')
+    for k in sorted(a_dictionary):
+        print(k)
