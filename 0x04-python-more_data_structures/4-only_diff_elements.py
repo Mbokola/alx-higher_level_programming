6afa87797cb765c103aa0b60572685b913294062
Mbokola <tonypeaks@gmail.com>
2023-05-20 10:43:35 +0000

    Task 4

---
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,10 @@
+#!/usr/bin/Python3
+'''
+- only_diff_elements - function that returns a set of all elements present in
+ only one set.
+- You are not allowed to import any module
+'''
+
+
+def only_diff_elements(set_1, set_2):
+    return set_1 ^ set_2
