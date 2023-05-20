081a40aa4f3b1fa77f25ff579a60d78ab587ba6c
Mbokola <tonypeaks@gmail.com>
2023-05-20 10:26:03 +0000

    Task 3

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,9 @@
+#!/usr/bin/Python3
+'''
+- common_elements - function that returns a set of common elements in two sets.
+- You are not allowed to import any module
+'''
+
+
+def common_elements(set_1, set_2):
+    return set_1 & set_2
