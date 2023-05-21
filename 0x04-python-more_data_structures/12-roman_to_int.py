1455f2b0f02bf95f84b7755f7fe1aae323080933
Mbokola <tonypeaks@gmail.com>
2023-05-21 08:35:09 +0000

    Task 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,17 +1,42 @@
-#!/usr/bin/python3
+#!/usr/bin/Python3
+'''
+- roman_to_int - function that converts a Roman numeral to an integer.
+
+- You can assume the number will be between 1 to 3999.
+- must return an integer
+- If the roman_string is not a string or None, return 0
+'''
+
+
 def roman_to_int(roman_string):
-    if (not len(roman_string)):
+    # Check if roman_string is None or not a string
+    if roman_string is None or type(roman_string) != str:
         return 0
+    # Craete a dictionary of roman values
     ref = dict(I=1, V=5, X=10, L=50, C=100, D=500, M=1000)
-    total = 0
-    counter = 0
-    for i in roman_string:
-        i = i.upper()
-        if i in ref:
-            total += ref[i]
-            if ref[i] > counter:
-                total -= counter * 2
-            counter = ref[i]
-        else:
+    # Check if roman String
+    for i in roman_string.upper():
+        if i not in ref:
             return 0
-    return total
+    length = len(roman_string)
+    # Check for lowercase roman string
+    new = roman_string.upper()
+    # Check if only 1 roman charcter
+    if (length == 1):
+        return ref[new]
+    conversion = 0
+    i = 0
+    # Iterate string length - 1 times
+    while i < length - 1:
+        # Handle IX/XI situations
+        if ref[new[i]] >= ref[new[i + 1]]:
+            conversion += ref[new[i]]
+            i += 1
+        else:
+            conversion += (ref[new[i + 1]] - ref[new[i]])
+            i += 2
+    # Make sure we get every character
+    if (i < length):
+        conversion += ref[new[i]]
+    # Return conversion
+    return conversion
--- 0x04-python-more_data_structures/test.py
+++ 0x04-python-more_data_structures/test.py
@@ -1,8 +1,19 @@
 #!/usr/bin/python3
-import ctypes
+""" Roman to Integer test file
+"""
+roman_to_int = __import__('12-roman_to_int').roman_to_int
 
-lib = ctypes.CDLL('./libPythonHBTN.so')
-lib.print_python_list.argtypes = [ctypes.py_object]
-lib.print_python_bytes.argtypes = [ctypes.py_object]
-b = b'What does the \'b\' character do in front of a string literal?';
-lib.print_python_bytes(b);
+roman_number = "X"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+roman_number = "VII"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+roman_number = "IX"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+roman_number = "LXXXVII"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+
+roman_number = "DCCVII"
+print("{} = {}".format(roman_number, roman_to_int(roman_number)))
