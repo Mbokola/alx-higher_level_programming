99e23ea08f8ea9eabc7804c6c270418907918c24
Mbokola <tonypeaks@gmail.com>
2023-05-29 15:34:25 +0000

    Task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     j = 0
-    for i in my_list:
-        if x:
-            print(i)
-            x -= 1
-            j += 1
+    try:
+        for i in my_list:
+            if (x):
+                print(x, end="")
+                x -= 1
+                j += 1
+        print()
+        return j
+    except Exception:
+        print("Exeption raised")
