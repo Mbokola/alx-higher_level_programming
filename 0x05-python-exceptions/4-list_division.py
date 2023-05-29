75eca74570033e3d57bb292ac172d1c3cffd45df
Mbokola <tonypeaks@gmail.com>
2023-05-29 19:42:23 +0000

    Task 4

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,18 @@
+def list_division(my_list_1, my_list_2, list_length):
+    new = []
+    comb = zip(my_list_1[:list_length], my_list_2[:list_length])
+    for k, v in comb:
+        try:
+            result = k / v
+        except TypeError:
+            result = 0
+            print("wrong type")
+        except ZeroDivisionError:
+            result = 0
+            print("division by 0")
+        except IndexError:
+            result = 0
+            print("out of range")
+        finally:
+            new.append(result)
+    return new
