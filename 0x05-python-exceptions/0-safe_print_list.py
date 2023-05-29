aa17c0277de5eb3a9824273bd7f2ac920224a4b9
Mbokola <tonypeaks@gmail.com>
2023-05-29 15:42:36 +0000

    Task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,7 +4,7 @@ def safe_print_list(my_list=[], x=0):
     try:
         for i in my_list:
             if (x):
-                print(x, end="")
+                print(i, end="")
                 x -= 1
                 j += 1
         print()
