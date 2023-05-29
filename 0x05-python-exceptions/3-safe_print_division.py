ca2045945a1de39153627eae22f0830edcaecbf9
Mbokola <tonypeaks@gmail.com>
2023-05-29 17:49:50 +0000

    Task 3

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -2,7 +2,8 @@
 def safe_print_division(a, b):
     try:
         result = a / b
-    except (ZeroDivisionError, FloatingPointError):
+    except Exception:
         result = None
     finally:
         print("Inside result: {}".format(result))
+        return result
