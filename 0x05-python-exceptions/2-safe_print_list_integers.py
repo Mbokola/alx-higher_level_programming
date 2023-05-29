3e1dbac21ac58df26b878c2c707a9c86a25e8258
Mbokola <tonypeaks@gmail.com>
2023-05-29 16:35:32 +0000

    Task 2

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    j = 0
+    for i in my_list:
+        if (x):
+            try:
+                print("{:d}".format(i), end="")
+                x -= 1
+                j += 1
+            except Exception:
+                pass
+    if (x):
+        raise IndexError
+    print()
+    return j
