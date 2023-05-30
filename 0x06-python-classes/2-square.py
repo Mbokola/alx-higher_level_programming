83c44802929df9667d2fbcef684e8c4f8cee82da
Mbokola <tonypeaks@gmail.com>
2023-05-30 18:45:28 +0000

    Task 2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" A simple Class """
+
+
+class Square:
+    """ The Square Class """
+    def __init__(self, size=0):
+        """ Class Instantiation """
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
