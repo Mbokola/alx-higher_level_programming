49f2124fb12d6956546472bc531e6f249e1ca18e
Mbokola <tonypeaks@gmail.com>
2023-05-30 18:19:21 +0000

    Task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" A simple Class """
+
+
+class Square:
+    """ The Square Class """
+    def __init__(self, size):
+        """ Class Instantiation """
+        self.__size = size
