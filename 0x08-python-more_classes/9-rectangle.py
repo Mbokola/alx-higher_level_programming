78252e729997b3a958c4385d93a287be65eca9e6
Mbokola <tonypeaks@gmail.com>
2023-06-05 22:23:08 +0000

    Task 9

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -86,4 +86,6 @@ class Rectangle:
 
     @classmethod
     def square(cls, size=0):
-        return cls(size, size)
+        instance = cls.__new__(cls)
+        instance.__init__(size, size)
+        return instance
