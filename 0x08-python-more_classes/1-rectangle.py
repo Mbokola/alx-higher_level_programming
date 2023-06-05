c81f7271e06fa739bbb946d30410e1e3329ec59e
Mbokola <tonypeaks@gmail.com>
2023-06-05 10:52:00 +0000

    Class initialization

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -8,14 +8,6 @@ class Rectangle:
     """
     def __init__(self, width=0, height=0):
         """ Instantiation with optional width and height """
-        if not isinstance(width, int):
-            raise TypeError("width must be an integer")
-        if width < 0:
-            raise ValueError("width must be >= 0")
-        if not isinstance(height, int):
-            raise TypeError("height must be an integer")
-        if height < 0:
-            raise ValueError("height must be >= 0")
         self.__height = height
         self.__width = width
 
@@ -26,6 +18,10 @@ class Rectangle:
 
     @height.setter
     def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
         """ Modifiy the height """
         self.__height = value
 
@@ -37,4 +33,8 @@ class Rectangle:
     @width.setter
     def width(self, value):
         """ Modify the width """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
         self.__width = value
