b8ff4a5a46661a2f6dfb13b94ee1db23120dc30c
Mbokola <tonypeaks@gmail.com>
2023-06-12 17:39:33 +0000

    Task 4

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" 4-inherits_from """
+
+
+def inherits_from(obj, a_class):
+    """ check if sub class """
+    return issubclass(type(obj), a_class) and type(obj) is not a_class
