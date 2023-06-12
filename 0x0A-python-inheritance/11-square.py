a4169698ac770c42669876d27377d5dbeca7446f
Mbokola <tonypeaks@gmail.com>
2023-06-12 20:46:29 +0000

    Task 11

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" 10-square module """
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """ The sqaure class """
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        self.__size = size
+        Rectangle.__init__(self, size, size)
+        Rectangle.area(self)
+
+    def __str__(self):
+        return f"[Square] {self.__size}/{self.__size}"
