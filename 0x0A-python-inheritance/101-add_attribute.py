22f6d08b92f61ecb76f1a4cc95f80309ce12430c
Mbokola <tonypeaks@gmail.com>
2023-06-12 21:19:06 +0000

    Task 13

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" 101-add_attribute module """
+
+
+def add_attribute(obj, attr, value):
+    if hasattr(obj, '__dict__'):
+        setattr(obj, attr, value)
+    else:
+        raise TypeError("can't add new attribute")
