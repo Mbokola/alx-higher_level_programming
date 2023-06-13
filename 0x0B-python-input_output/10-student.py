18633c24bf1975264103ce992f440a551a86ee85
Mbokola <tonypeaks@gmail.com>
2023-06-13 19:56:40 +0000

    Task 10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" 9-student module """
+
+
+class Student:
+    """ The Student class """
+    def __init__(self, first_name, last_name, age):
+        """ Class initialization """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ Dictionary representation of class atrributes """
+        mydict = {}
+        if attrs:
+            if ("first_name" in attrs):
+                mydict["first_name"] = self.first_name
+            if ("last_name" in attrs):
+                mydict["last_name"] = self.last_name
+            if ("age" in attrs):
+                mydict["age"] = self.age
+            return mydict
+        return self.__dict__
