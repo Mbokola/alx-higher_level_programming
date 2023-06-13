8b1ba127d7813c06447b90728f59b5f0f7b77dcd
Mbokola <tonypeaks@gmail.com>
2023-06-13 20:37:01 +0000

    Task 11

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" 9-student module """
+
+
+class Student:
+    """ The Student class """
+    def __init__(self, first_name, last_name, age):
+        """ Class initialization """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ Dictionary representation of class atrributes """
+        if attrs is not None:
+            mydict = {}
+            if ("first_name" in attrs):
+                mydict["first_name"] = self.first_name
+            if ("last_name" in attrs):
+                mydict["last_name"] = self.last_name
+            if ("age" in attrs):
+                mydict["age"] = self.age
+            return mydict
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """ replaces all attributes of the Student instance """
+        for i in json:
+            self.__dict__.update({i: json[i]})
