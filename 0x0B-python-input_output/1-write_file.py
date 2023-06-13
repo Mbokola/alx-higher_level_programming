1f8eb4347afdf712015f36d6a797e844dc6df972
Mbokola <tonypeaks@gmail.com>
2023-06-13 13:35:25 +0000

    Task 1

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -3,5 +3,6 @@
 
 
 def write_file(filename="", text=""):
+    """ Write to file, create if non-existent else overwrite"""
     with open(filename, 'w', encoding="utf-8") as f:
         return f.write(text)
