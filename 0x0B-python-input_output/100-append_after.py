c00d11fc5d152a8f729b0988fd4a568c0119e33e
Mbokola <tonypeaks@gmail.com>
2023-06-14 11:41:55 +0000

    Task 13

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" 100-append_after module """
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """ inserts a line of text to a file, after each line
+        containing a specific string """
+    with open(filename, 'r', encoding='utf-8') as f:
+        lines = f.readlines()
+    with open(filename, 'w', encoding='utf-8') as f:
+        for line in lines:
+            f.write(line)
+            if search_string in line:
+                f.write(new_string)
