4dbcab869d420fd1a1f45ae9a94e28f7d9548d10
Mbokola <tonypeaks@gmail.com>
2023-07-18 21:42:50 +0000

    Task 15

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,6 +1,6 @@
 -- lists the number of records with the same score in the table second_table of the database hbtn_0c_0
 -- the number of records for this score with the label number
 
-select score, count(*) AS number FROM second_table
+SELECT score, count(*) AS number FROM second_table
 GROUP BY score
 ORDER BY number DESC;
