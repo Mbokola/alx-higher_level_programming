e9e65b3feaf826456d9ced4fbec2bdc67f2fc3e2
Mbokola <tonypeaks@gmail.com>
2023-07-20 14:10:07 +0000

    Task 4

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -1,3 +1,3 @@
 -- creates the table id_not_null on your MySQL server.
 
-CREATE TABLE IF NOT EXISTS ID_NOT_NULL (id INT (1), name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS id_not_null (id INT (1), name VARCHAR(256));
