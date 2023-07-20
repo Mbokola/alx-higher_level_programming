2e7d7a21414eb129e8a95a719337931681027d87
Mbokola <tonypeaks@gmail.com>
2023-07-20 18:33:42 +0000

    Task 7

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -2,6 +2,6 @@
 CREATE DATABASE hbtn_0d_usa;
 USE hbtn_0d_usa;
 CREATE TABLE cities (id INT AUTO_INCREMENT PRIMARY KEY,
-       	     	    state_id INT,
+       	     	    state_id INT NOT NULL,
 		    name VARCHAR(256) NOT NULL,
 		    FOREIGN KEY (state_id) REFERENCES states(id));
