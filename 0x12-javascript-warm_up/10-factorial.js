2e9af4f87878112f8ef7af6c9d135739545b8673
Mbokola <tonypeaks@gmail.com>
2023-08-14 21:05:17 +0000

    Task 10

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,15 +1,15 @@
 #!/usr/bin/node
 
-function factorial(n) {
-    if (n === 0 || n === 1) {
-	return (1);
-    } else {
-	return n * factorial(n-1);
-    }
+function factorial (n) {
+  if (n === 0 || n === 1) {
+    return (1);
+  } else {
+    return n * factorial(n - 1);
+  }
 }
 const arg = Number(process.argv[2]);
 if (!arg) {
-    console.log(1);
+  console.log(1);
 } else {
-    console.log(factorial(arg));
+  console.log(factorial(arg));
 }
