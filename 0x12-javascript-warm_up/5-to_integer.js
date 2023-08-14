17057c6a2f7b2e42d4c4fd44bd9e87971e4f39ea
Mbokola <tonypeaks@gmail.com>
2023-08-14 19:15:52 +0000

    Readable

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,6 +1,8 @@
 #!/usr/bin/node
 
 const arg = process.argv[2];
-if (isNaN(arg)) { console.log('Not a number'); } else {
+if (isNaN(arg)) {
+  console.log('Not a number');
+} else {
   console.log('My number: ' + arg);
 }
