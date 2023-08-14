bf279890041f54f4ada91a28c1c15b3a3e817332
Mbokola <tonypeaks@gmail.com>
2023-08-14 21:19:26 +0000

    Task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,8 +2,8 @@
 
 const args = process.argv.slice(2).map(Number);
 if (!args[3]) {
-    console.log(0);
+  console.log(0);
 } else {
-    args.sort((a, b) => b - a);
-    console.log(args[1]);
+  args.sort((a, b) => b - a);
+  console.log(args[1]);
 }
