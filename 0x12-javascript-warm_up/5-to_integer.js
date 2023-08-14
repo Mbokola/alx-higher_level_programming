73f21c89261e684958640fb4ec7cc260dfeb1651
Mbokola <tonypeaks@gmail.com>
2023-08-14 19:04:34 +0000

    Task 5

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,13 +1,6 @@
 #!/usr/bin/node
 
-const len = process.argv.length;
-if (len >= 3) {
-  const converted = Number(process.argv[2]);
-  if (isNaN(converted)) {
-    console.log('Not a number');
-  } else {
-    console.log('My number: ' + converted);
-  }
-} else {
-  console.log('Not a number');
+const arg = process.argv[2];
+if (isNaN(arg)) { console.log('Not a number'); } else {
+  console.log('My number: ' + arg);
 }
