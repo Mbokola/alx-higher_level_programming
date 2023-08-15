93dd38af5b7e21f7bf531484bec6f757de63a53e
Mbokola <tonypeaks@gmail.com>
2023-08-15 22:56:15 +0000

    import correct file

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-const Rectangle = require('./4-rectangle'); // import the rectangle class
+const Rectangle = require('./5-square'); // import the rectangle class
 class Square extends Rectangle { // Write a class Square that defines a square and inherits from Rectangle
   constructor (size) { // The constructor must take 1 argument: size
     super(size, size); // The constructor of Rectangle must be called (by using super())
