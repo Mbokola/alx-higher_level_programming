a07ae9bfe9b4a5281bb22d8ce5f6ad4c8dbcd9bf
Mbokola <tonypeaks@gmail.com>
2023-08-18 11:05:00 +0000

    Task 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
-
-import sys
-import MySQLdb
-
-mysql_username = sys.argv[1]
-mysql_password = sys.argv[2]
-database_name = sys.argv[3]
-host = 'localhost'
-
 if __name__ == '__main__':
+    import sys
+    import MySQLdb
+
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+    host = 'localhost'
+
     db = MySQLdb.connect(host=host, user=mysql_username, passwd=mysql_password,
                          db=database_name, port=3306)
 
