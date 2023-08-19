6ffae9acf039c56b11f664e496c45a6b062afc20
Mbokola <tonypeaks@gmail.com>
2023-08-19 08:22:09 +0000

    Task 8

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-"""Start link class to table in database
-"""
-import sys
-from model_state import Base, State
-
-from sqlalchemy import (create_engine)
-from sqlalchemy.orm import sessionmaker
+""" Start link class to table in database """
 
 if __name__ == "__main__":
+    import sys
+    from model_state import Base, State
+
+    from sqlalchemy import (create_engine)
+    from sqlalchemy.orm import sessionmaker
+
     engine = create_engine(f'mysql+mysqldb://{sys.argv[1]}:{sys.argv[2]}@\
 localhost/{sys.argv[3]}', echo=False)
     Base.metadata.create_all(engine)
