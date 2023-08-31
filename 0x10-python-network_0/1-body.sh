41f61d800f3a1abe6f2546540272d4f446f5e674
Mbokola <tonypeaks@gmail.com>
2023-08-31 19:02:37 +0000

    Simpler

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
-curl -s -f -L "%{http_code}" -o - "$1"
+curl -s -f -L "$1"
