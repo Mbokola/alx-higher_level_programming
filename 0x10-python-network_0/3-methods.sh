69cb0dd61b3fd7e24eac806e8a23903055fee513
Mbokola <tonypeaks@gmail.com>
2023-08-31 19:34:30 +0000

    Try sed

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Bash script that takes in a URL and displays all HTTP methods the server will accept.
 
-curl -sI -X HEAD "$1" | awk -F ': ' '/Allow/ {print $2}'
+curl -sI -X OPTIONS "$1" | sed -n 's/^Allow: \(.*\)/\1/p'
