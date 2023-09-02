229c93b855b796f10c9143b599c1034c0e71fd47
Mbokola <tonypeaks@gmail.com>
2023-09-02 18:41:40 +0000

    use with

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 """ 0-hbtn_status module """
+import urllib.request
 
 if __name__ == '__main__':
-    import urllib.request
     url = 'https://alx-intranet.hbtn.io/status'
-    request = urllib.request.Request(url)
-    response = urllib.request.urlopen(request)
-    content = response.read()
+    with urllib.request.urlopen(url) as response:
+        content = response.read()
 
     print('Body response:')
     print(f'\t - type: {type(content)}')
