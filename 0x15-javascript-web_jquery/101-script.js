3d7dda34d145ba0ee8af887b63cea0e641097e5f
Mbokola <tonypeaks@gmail.com>
2023-10-04 16:52:40 +0000

    Task 11

---
+++ 0x15-javascript-web_jquery/101-script.js
@@ -0,0 +1,5 @@
+$(function () {
+  $('#add_item').click(() => $('<li>Item</li>').appendTo('.my_list'));
+  $('#remove_item').click(() => $('.my_list li:last-child').remove());
+  $('#clear_list').click(() => $('.my_list').empty());
+});
