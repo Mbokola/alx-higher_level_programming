8f2ec828a70178cae2a076bfd807f858ef987143
Mbokola <tonypeaks@gmail.com>
2023-10-04 16:49:00 +0000

    Task 10

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,5 @@
+// updates the text color of the <header> element to red (#FF0000):
+window.onload = () => {
+  const header = document.querySelector('header');
+  if (header) header.style.color = '#FF0000';
+};
