33f3e0e9b3872142d9916ef026686772b10a9a6b
Mbokola <tonypeaks@gmail.com>
2023-10-04 14:42:10 +0000

    Task 7

--- 0x15-javascript-web_jquery/7-script.js
+++ 0x15-javascript-web_jquery/7-script.js
@@ -1,4 +1,4 @@
-// adds the class red to the <header> element when the user clicks on the tag
+//fetches the character name from this URL: https://swapi-api.alx-tools.com/api/people/5/?format=json
 $.ajax({
   url: 'https://swapi-api.alx-tools.com/api/people/5/?format=json',
   dataType: 'json',
