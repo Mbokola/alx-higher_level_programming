4e2a6d5296ecc0265e5f8e5c6a4a108dc389f2bc
Mbokola <tonypeaks@gmail.com>
2023-10-04 14:42:43 +0000

    Task 6

--- 0x15-javascript-web_jquery/6-script.js
+++ 0x15-javascript-web_jquery/6-script.js
@@ -1,4 +1,4 @@
-// adds the class red to the <header> element when the user clicks on the tag
+//updates the text of the <header> element to New Header!!! when the user clicks on DIV#update_header
 $(document).on('click', '#update_header', function () {
   $('header').text('New Header!!!');
 });
