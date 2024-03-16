b24734c95b1b4c852b89740909150956de23b498
Mbokola <tonypeaks@gmail.com>
2024-03-16 09:25:25 +0000

    semistandard compliance

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -2,7 +2,7 @@
 
 const request = require('request');
 const number = process.argv[2];
-const api_url = `https://swapi-api.alx-tools.com/api/films/${number}`;
+const apiUrl = `https://swapi-api.alx-tools.com/api/films/${number}`;
 
 request(api_url, { json: true }, (err, response, body) => {
   if (err) console.log(err);
