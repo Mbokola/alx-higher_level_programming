ad1bc4aaa3f354cfc57b5b283bd8a68dbf1d5ba6
Mbokola <tonypeaks@gmail.com>
2024-03-16 09:26:24 +0000

    update variable

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -4,7 +4,7 @@ const request = require('request');
 const number = process.argv[2];
 const apiUrl = `https://swapi-api.alx-tools.com/api/films/${number}`;
 
-request(api_url, { json: true }, (err, response, body) => {
+request(apiUrl, { json: true }, (err, response, body) => {
   if (err) console.log(err);
   console.log(body.title);
 });
