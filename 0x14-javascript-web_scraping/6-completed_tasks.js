96e24740d4778e594359ce325f30b2ec7c32ded3
Mbokola <tonypeaks@gmail.com>
2024-03-19 10:57:19 +0000

    Semistandard

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -5,9 +5,13 @@ const request = require('request');
 const url = process.argv[2];
 
 request(url, { json: true }, (err, res, body) => {
+  if (err) {
+    console.err(err);
+    return;
+  }
   const obj = {};
-  body.map((user) => {
-    if (obj.hasOwnProperty(user.userId)) {
+  body.forEach((user) => {
+    if (Object.prototype.hasOwnProperty.call(obj, user.userId)) {
       if (user.completed) {
         obj[user.userId] += 1;
       }
