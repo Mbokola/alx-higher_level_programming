c7ce269e97e588887db08c2a02315115510c0241
Mbokola <tonypeaks@gmail.com>
2024-03-20 08:09:20 +0000

    Right order

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -5,18 +5,29 @@ const request = require('request');
 const movieId = process.argv[2];
 const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
 
-request(url, { json: true }, (err, res, body) => {
+request(url, { json: true }, async (err, res, body) => {
   if (err) {
     console.err(err);
     return;
   }
-  body.characters.forEach(async (character) => {
-    await request(character, { json: true }, (err, res, body) => {
-      if (err) {
-        console.log(err);
-        return;
-      }
+  for (const characterUrl of body.characters) {
+    try {
+      const body = await getRequest(characterUrl)
       console.log(body.name);
+    } catch (err) {
+      console.error(err);
+    }
+  }
+});
+
+function getRequest(url) {
+  return new Promise((resolve, reject) => {
+    request(url, { json: true } , (err, res, body) => {
+      if (err) {
+        reject(err)
+      } else {
+        resolve(body)
+      }
     });
   });
-});
+};
